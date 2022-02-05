69ddaf150b66268582d0d7af48277790231d7219
Emanuel Sandries - ESandries <hello.esandries@gmail.com>
2022-02-04 23:52:51 -0600

    help me

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,17 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from "../components/Header";
 import AvoProject from "../projects/avo-Project-React/AvoProject";
 import '../main.css'
-import Layout from "../components/Layout";
 
 const App = () => {
     return (
         <div>
             <Header></Header>
-            <BrowserRouter BrowserRouter >
+            <AvoProject></AvoProject>
+            {/* <BrowserRouter BrowserRouter >
                 <Routes>
                     <Route exact path="/" element={<AvoProject />} />
                 </Routes>
-            </BrowserRouter >
+            </BrowserRouter > */}
         </div>
     );
 };
